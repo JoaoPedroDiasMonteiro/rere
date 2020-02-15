5f817a95a4c885aeb3a910c8fc2a2334c2eeb5b6
João Pedro Dias <56093063+JoaoPedroDiasMonteiro@users.noreply.github.com>
2020-02-15 03:30:20 -0300

    Add files via upload

---
+++ precache-manifest.acd253f176c16b4b308ef5a14904152c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ca7c40b7e81a2c3b7cfd4c31c9422c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbe67d293f4d7192ceec",
+    "url": "/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "e2ad7f7b69a903beef36",
+    "url": "/static/css/main.c147c5c8.chunk.css"
+  },
+  {
+    "revision": "dbe67d293f4d7192ceec",
+    "url": "/static/js/2.aba8a4f3.chunk.js"
+  },
+  {
+    "revision": "004c8ddb448ab3a8a4574e6f18483e3a",
+    "url": "/static/js/2.aba8a4f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2ad7f7b69a903beef36",
+    "url": "/static/js/main.f5643a87.chunk.js"
+  },
+  {
+    "revision": "b0ec6daa1808138c7a12",
+    "url": "/static/js/runtime-main.9cd6e0bf.js"
+  },
+  {
+    "revision": "3774879e19b714a795d2922532e7debc",
+    "url": "/static/media/ELDA-MARIA.3774879e.TTF"
+  },
+  {
+    "revision": "ca5e332738946e1d04344029762239b6",
+    "url": "/static/media/black-brown-and-white-melted-cake-on-round-white-ceramic-954199.ca5e3327.jpg"
+  },
+  {
+    "revision": "2d54212e5e6c6e4821b16ba93389b2c3",
+    "url": "/static/media/blueberries-cake-chocolate-chocolate-cake-291528.2d54212e.jpg"
+  },
+  {
+    "revision": "d66ecdc37ef438c38895e0da986db62b",
+    "url": "/static/media/blur-breakfast-close-up-dairy-product-376464.d66ecdc3.jpg"
+  },
+  {
+    "revision": "a2bc8024b49696be0a183b91ede94b31",
+    "url": "/static/media/bowl-breakfast-calcium-cereal-414262.a2bc8024.jpg"
+  },
+  {
+    "revision": "118f94936cd2c1e462fb1d0b79beb71a",
+    "url": "/static/media/chocolate-beside-raspberry-918328.118f9493.jpg"
+  },
+  {
+    "revision": "4df628dc503106cd46a480d68714bb84",
+    "url": "/static/media/chocolate-cake-on-chopping-board-2067396.4df628dc.jpg"
+  },
+  {
+    "revision": "2aea2f3088f4f7aefb681b437ee79c41",
+    "url": "/static/media/colheres.2aea2f30.jpg"
+  },
+  {
+    "revision": "b600fbd97d50f40c3299968bd5e64b71",
+    "url": "/static/media/colorful-doughnuts-3338681.b600fbd9.jpg"
+  },
+  {
+    "revision": "09883d1986df017192f026ba65b853e6",
+    "url": "/static/media/strawberries-1132558.09883d19.jpg"
+  },
+  {
+    "revision": "a4fc43e4c48759cb631766167420111e",
+    "url": "/static/media/white-bowl-of-whole-strawberries-89778.a4fc43e4.jpg"
+  }
+]);
